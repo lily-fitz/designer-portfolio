c45964a5d95d38b1aef9a25bbd7e3580bd646345
Lily Fitz <lilymfitz@gmail.com>
2022-09-03 20:02:09 -0400

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 //sticky header
 
 $(function () {
-    $(window).on('scroll', function () {
-        if ($(window).scrollTop() > 50) {
-            $('.lily-sticky-header').addClass('active');
-        } else {
-            $('.lily-sticky-header').removeClass('active');
-        }
-    });
-});
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() > 50) {
+      $('.lily-sticky-header').addClass('active')
+    } else {
+      $('.lily-sticky-header').removeClass('active')
+    }
+  })
+})
 
 //https://www.w3schools.com/howto/howto_css_fixed_sidebar.asp
