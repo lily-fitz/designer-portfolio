5029013911370d7b07a0e5ca4a3a3de3297bf7ae
Lily Fitz <lilymfitz@gmail.com>
2022-09-04 10:02:31 -0400

    fixing accidental push

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,25 @@ $(function () {
 })
 
 //https://www.w3schools.com/howto/howto_css_fixed_sidebar.asp
+
+// const introWelcome = document.querySelectorAll('#intro__svg-welcome path')
+
+// for (let i = 0; i < localStorage.length; i++) {
+//   console.log(i)
+// }
+
+//const intro = document.getElementById(intro)
+//const introElements = document.querySelectorAll('#intro > *')
+
+// window.addEventListener('scroll', () => {
+//   const value = window.scrollY
+//   console.log(value)
+//   //intro.style.opacity = 100 - value + '%'
+// })
+
+// const scrollContainer = document.getElementById('projects')
+
+// scrollContainer.addEventListener('wheel', (evt) => {
+//   evt.preventDefault()
+//   scrollContainer.scrollLeft += evt.deltaY
+// })
